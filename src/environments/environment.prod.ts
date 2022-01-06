c2e06e33a0ddfadfe78f13fc3310bce321e40690
Lyan Dwi Pangestu <gloriancrack@gmail.com>
2022-01-06 12:18:30 +0700

    fix: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://api.aptfi.or.id',
+  api: 'https://api.aptfi.or.id/api',
   recaptchaSiteKey: '6LdTG_QdAAAAALFFcUrb34BTJACNIfcocAhbnX3w',
 };
